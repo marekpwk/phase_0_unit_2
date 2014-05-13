ba24f4885741dfc64d69a9e426dbe75de2244ddd
Mar <rkx1901@gmail.com>
2014-05-13 16:52:43 -0500

    Refactored solution

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -30,32 +30,40 @@
 # END IF
 # RETURN new_string
 # 2. Initial Solution
-def separate_comma(number)
-  number_string = number.to_s.split("")
-  new_string = ""
-  if number_string.length < 4
-    return number_string.join
-  else
-    sub_str = ""
-    number_string.reverse.each do |digit|
-      sub_str  = digit + sub_str
-      if sub_str.length == 3
-        new_string = "," + sub_str + new_string
-        sub_str = ""
-      end
-    end
-    new_string = sub_str << new_string
-    if new_string[0] == ","
-      new_string = new_string[1..-1]
-    end
-  end
-  new_string
-end
+# def separate_comma(number)
+#   number_string = number.to_s.split("")
+#   new_string = ""
+#   if number_string.length < 4
+#     return number_string.join
+#   else
+#     sub_str = ""
+#     number_string.reverse_each do |digit|
+#       sub_str  = digit + sub_str
+#       if sub_str.length == 3
+#         new_string = "," + sub_str + new_string
+#         sub_str = ""
+#       end
+#     end
+#     new_string = sub_str << new_string
+#     if new_string[0] == ","
+#       new_string = new_string[1..-1]
+#     end
+#   end
+#   new_string
+# end
 
-puts separate_comma(123456789)
 
 # 3. Refactored Solution
-
+def separate_comma(number)
+  number_array = number.to_s.split("")
+  sliced_array = []
+  formatted_number = ""
+  number_array.reverse.each_slice(3){|numbers| sliced_array << numbers}
+  sliced_array.reverse.each_with_index do |numbers, index|
+    index == sliced_array.length - 1 ? formatted_number << numbers.reverse.join : formatted_number << numbers.reverse.join << ","
+  end
+  formatted_number
+end
 
 
 # 4. Reflection
