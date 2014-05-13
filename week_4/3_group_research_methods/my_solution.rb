5d384ae0803bba8b009f148beef642eccdf277c5
Mar <rkx1901@gmail.com>
2014-05-13 18:24:46 -0500

    Added part 4 of solution

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W4: Research Methods
 
 i_want_pets = ["I", "want", 3, "pets", "but", "I", "only", "have", 2]
-my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, 
+my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4,
             "Annabelle" => 0, "Ditto" => 3}
 
 # Person 1's solution
@@ -13,8 +13,8 @@ def my_hash_finding_method(source, thing_to_find)
   # Your code here!
 end
 
-# Identify and describe the ruby method you implemented. 
-# 
+# Identify and describe the ruby method you implemented.
+#
 #
 #
 
@@ -27,8 +27,8 @@ def my_hash_modification_method(source, thing_to_modify)
   # Your code here!
 end
 
-# Identify and describe the ruby method you implemented. 
-# 
+# Identify and describe the ruby method you implemented.
+#
 #
 #
 
@@ -42,29 +42,28 @@ def my_hash_sorting_method(source)
   # Your code here!
 end
 
-# Identify and describe the ruby method you implemented. 
-# 
+# Identify and describe the ruby method you implemented.
+#
 #
 #
 
 
 # Person 4
 def my_array_deletion_method(source, thing_to_delete)
-  #Your code here!
+  source.delete_if { |word| word.to_s.include?(thing_to_delete) }
 end
 
 def my_hash_deletion_method(source, thing_to_delete)
-  #Your code here!
+  source.delete_if { |key| key.to_s.include?(thing_to_delete) }
 end
 
-# Identify and describe the ruby method you implemented. 
-# 
-#
-#
-
+# Identify and describe the ruby method you implemented.
+# I've use delete_if method, which takes the block, and deletes element when block evaluates to true.
+# Second method I've used is to_s which converst an element to a string
+# Third method I've used is include? which returns true  if string contains the given string or character
 
 ################## DRIVER CODE ###################################
-# HINT: Use `puts` statements to see if you are altering the original structure with these methods. 
+# HINT: Use `puts` statements to see if you are altering the original structure with these methods.
 # Each of these should return `true` if they are implemented properly.
 
 p my_array_finding_method(i_want_pets, "t") == ["want","pets","but"]
@@ -72,14 +71,14 @@ p my_hash_finding_method(my_family_pets_ages, 3) == ["Hoobie", "Ditto"]
 p my_array_modification_method(i_want_pets, 1) == ["I", "want", 4, "pets", "but", "I", "only", "have", 3 ]
 p my_hash_modification_method(my_family_pets_ages, 2) == {"Evi" => 8, "Hoobie" => 5, "George" => 14, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
 p my_array_sorting_method(i_want_pets) == ["3", "4", "I", "but", "have", "only", "pets", "want"]
-p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]] 
-# This may be false depending on how your method deals with ordering the animals with the same ages.
+p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]]
+This may be false depending on how your method deals with ordering the animals with the same ages.
 p my_array_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "I", "only", 3 ]
 p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
 
 # Reflect!
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
