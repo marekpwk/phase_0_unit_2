790a5b3fe27dc23c9e8b0d9cf29c4cc5a1af34ad
Mar <rkx1901@gmail.com>
2014-05-12 23:37:30 -0500

    Added solution

--- week_4/2_creative/create_accountability_groups/my_solution.rb
+++ week_4/2_creative/create_accountability_groups/my_solution.rb
@@ -5,32 +5,41 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: method takes an arary of names
+# Output: will return number of arrays with names in it for different groups
 # Steps:
-
+# DEF method create_group which takes an array as an argument
+# Shuffle the array and assign it to variable shuffled_array
+# SET variable new_groups to an empty array
+# LOOP through shuffled_array
+#   ADD Every four elements as an array to  new_groups
+# END LOOP
+# RETURN new_groups
 
 # 3. Initial Solution
-
-
-
+def new_groups(array)
+ new_group = []
+ array.shuffle.each_slice(4){|acc| new_group << acc}
+  if new_group.length > 1 && new_group.last.length <= 2
+    (new_group.last.length).times do |i|
+      new_group[i].push(new_group.last.pop)
+      new_group.pop
+    end
+  end
+ new_group
+end
 
 # 4. Refactored Solution
 
 
 
-
-
-
-
-
 # 1. DRIVER TESTS GO BELOW THIS LINE
+puts new_groups(["a","b","c","d","e","f","g","h"])[0].length == 4 #returns true if lenght of 1st group is 4
+puts new_groups(["a","b","c","d","e","f", "g"])[1].length == 3   #returns true if lenght of  group is 3
+puts new_groups(["a","b","c","d","e"])[0].length == 5       #returns true if lenght of 1st group is 5
 
 
 
-
-
-
-# 5. Reflection 
+# 5. Reflection
 
 
