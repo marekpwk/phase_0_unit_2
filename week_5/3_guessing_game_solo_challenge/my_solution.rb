892bafaa60da32200f55542262c8eb5ea2057582
Mar <rkx1901@gmail.com>
2014-05-20 15:02:04 -0500

    Added solution and drivers code

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -5,36 +5,98 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: INIT class GuessingGame with variable answer
+# Output: Object should respond to methods guesssolved?
 # Steps:
-
+# INIT with answer
+#   SET solved to false
+# END INIT
+# DEF guess which takes a num as argument
+#   IF num equals @answer
+#     SET solved to true and return :correct
+#   ELSEIF num is more than @answer
+#     SET solved to false and return :hih
+#   ELSEIF num is less than @answer
+#     SET solved to false and return :low
+#   END IF
+# END
+# DEF solved?
+#   will return solved
+# END
 
 # 3. Initial Solution
 
 class GuessingGame
-  def initialize(answer)
-    # Your initialization code goes here
-  end
-  
-  # Make sure you define the other required methods, too
-end
 
+  def initialize(answer)
+    @answer = answer
+    @solved = false
+  end
+
+  def guess(num)
+
+      if num == @answer
+        @solved = true
+        :correct
+      elsif num > @answer
+        @solved = false
+        :high
+      else
+        @solved = false
+        :low
+      end
+  end
+
+  def solved?
+    @solved
+  end
+
+end
 
 
 
 # 4. Refactored Solution
 
+class GuessingGame
 
+  def initialize(answer)
+    @answer = answer
+    @solved = false
+  end
+
+  def guess(num)
+    @solved = num == @answer
+    if num == @answer
+      :correct
+    elsif num > @answer
+     :high
+    else
+     :low
+    end
+  end
+
+  def solved?
+    @solved
+  end
+
+
+end
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+game = GuessingGame.new(4)
+puts game.solved? == false
+puts game.guess(2) == :low
+puts game.solved? == false
+puts game.guess(10) == :high
+puts game.solved? == false
+puts game.guess(4) == :correct
+puts game.solved? == true
 
 
 
 
 
-
-# 5. Reflection 
+# 5. Reflection
