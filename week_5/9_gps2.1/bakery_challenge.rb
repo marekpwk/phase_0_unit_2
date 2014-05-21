5201f116c2f574e7fc24c37cf576dde5e9180f28
Mar <rkx1901@gmail.com>
2014-05-20 20:27:07 -0500

    Refactored

--- week_5/9_gps2.1/bakery_challenge.rb
+++ week_5/9_gps2.1/bakery_challenge.rb
@@ -1,54 +1,138 @@
+# # U2.W5: The Bakery Challenge (GPS 2.1)
+
+# # Your Names
+# # 1)
+# # 2)
+
+#  # This is the file you should end up editing.
+
+# def bakery_num(num_of_people, fav_food)
+#   my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
+#   pie_qty = 0
+#   cake_qty = 0
+#   cookie_qty = 0
+
+#   has_fave = false
+
+#   my_list.each_key do |k|
+#   if k == fav_food
+#   has_fave = true
+#   fav_food = k
+#   end
+#   end
+#   if has_fave == false
+#   raise ArgumentError.new("You can't make that food")
+#   else
+#   fav_food_qty = my_list.values_at(fav_food)[0]
+#   if num_of_people % fav_food_qty == 0
+#     num_of_food = num_of_people / fav_food_qty
+#     return "You need to make #{num_of_food} #{fav_food}(s)."
+#   else num_of_people % fav_food_qty != 0
+#     while num_of_people > 0
+#     if num_of_people / my_list["pie"] > 0
+#     pie_qty = num_of_people / my_list["pie"]
+#     num_of_people = num_of_people % my_list["pie"]
+#     elsif num_of_people / my_list["cake"] > 0
+#     cake_qty = num_of_people / my_list["cake"]
+#     num_of_people = num_of_people % my_list["cake"]
+#     else
+#     cookie_qty = num_of_people
+#     num_of_people = 0
+#     end
+#     end
+#     return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
+#     end
+#   end
+# end
+
+
+# #-----------------------------------------------------------------------------------------------------
+# #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
+# # These are the tests to ensure it's working.
+# # These should all print true if the method is working properly.
+# p bakery_num(24, "cake") == "You need to make 4 cake(s)."
+# p bakery_num(41, "pie") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)."
+# p bakery_num(24, "cookie") == "You need to make 24 cookie(s)."
+# p bakery_num(4, "pie") == "You need to make 0 pie(s), 0 cake(s), and 4 cookie(s)."
+# p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie(s)."
+# # p bakery_num(3, "apples") # this will raise an ArgumentError
+
+# # You SHOULD change this driver code. Why? Because it doesn't make sense.
+# p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
+
+#
 # U2.W5: The Bakery Challenge (GPS 2.1)
 
-# Your Names
-# 1)
-# 2)
+# def bakery_num(num_of_people, fav_food)
+#   my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
+#   pie_qty = 0
+#   cake_qty = 0
+#   cookie_qty = 0
 
- # This is the file you should end up editing. 
- 
+#   has_fave = false
+
+#   my_list.each_key do |k|
+#     if k == fav_food
+#        has_fave = true
+#     end
+#   end
+#   if has_fave == false
+#     raise ArgumentError.new("You can't make that food")
+#   else
+#     fav_food_qty = my_list[fav_food]
+#     if num_of_people % fav_food_qty == 0
+#       num_of_food = num_of_people / fav_food_qty
+#       num_of_people = 0
+#       return "You need to make #{num_of_food} #{fav_food}(s)."
+#     end
+#     # else num_of_people % fav_food_qty != 0
+#     while num_of_people > 0
+#       food_array = []
+#       my_list.each do |k, v|
+#         food_qty = num_of_people / v
+#         num_of_people = num_of_people % v
+#         food_array << food_qty
+#       end
+#       # end
+#       return "You need to make #{food_array[0]} pie(s), #{food_array[1]} cake(s), and #{food_array[2]} cookie(s)."
+#     end
+#   end
+# end
 def bakery_num(num_of_people, fav_food)
   my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
   pie_qty = 0
   cake_qty = 0
   cookie_qty = 0
-  
   has_fave = false
+  has_fave = true  if my_list.has_key? fav_food
 
-  my_list.each_key do |k|
-  if k == fav_food
-  has_fave = true
-  fav_food = k
-  end
-  end
   if has_fave == false
-  raise ArgumentError.new("You can't make that food")
+    raise ArgumentError.new("You can't make that food")
   else
-  fav_food_qty = my_list.values_at(fav_food)[0]
-  if num_of_people % fav_food_qty == 0
-    num_of_food = num_of_people / fav_food_qty
-    return "You need to make #{num_of_food} #{fav_food}(s)."
-  else num_of_people % fav_food_qty != 0
+    fav_food_qty = my_list[fav_food]
+    if num_of_people % fav_food_qty == 0
+      num_of_food = num_of_people / fav_food_qty
+      num_of_people = 0
+      return "You need to make #{num_of_food} #{fav_food}(s)."
+    end
+    # else num_of_people % fav_food_qty != 0
     while num_of_people > 0
-    if num_of_people / my_list["pie"] > 0
-    pie_qty = num_of_people / my_list["pie"]
-    num_of_people = num_of_people % my_list["pie"]
-    elsif num_of_people / my_list["cake"] > 0
-    cake_qty = num_of_people / my_list["cake"]
-    num_of_people = num_of_people % my_list["cake"]
-    else
-    cookie_qty = num_of_people
-    num_of_people = 0
-    end
-    end
-    return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
+      food_array = []
+      my_list.each do |k, v|
+        food_qty = num_of_people / v
+        num_of_people = num_of_people % v
+        food_array << food_qty
+      end
+      # end
+      return "You need to make #{food_array[0]} pie(s), #{food_array[1]} cake(s), and #{food_array[2]} cookie(s)."
     end
   end
 end
- 
+
 
 #-----------------------------------------------------------------------------------------------------
 #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
-# These are the tests to ensure it's working. 
+# These are the tests to ensure it's working.
 # These should all print true if the method is working properly.
 p bakery_num(24, "cake") == "You need to make 4 cake(s)."
 p bakery_num(41, "pie") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)."
@@ -59,5 +143,3 @@ p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie
 
 # You SHOULD change this driver code. Why? Because it doesn't make sense.
 p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
- 
- 
