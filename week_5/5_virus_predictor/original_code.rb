e19953648447cfca2ebed813173119f557de146b
Mar <rkx1901@gmail.com>
2014-05-23 20:08:34 -0500

    Refactored method speed_of_speed

--- week_5/5_virus_predictor/original_code.rb
+++ week_5/5_virus_predictor/original_code.rb
@@ -1,12 +1,12 @@
 # U2.W5: Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself, with: Adrian Tawadros].
 
 # EXPLANATION OF require_relative
-#
-#
-require_relative 'state_data'
-#Saying that we need the file state_date to work
+#It allows to load a file which is in realtive path to the file containing require_relative
+
+require_relative 'state_data' #Saying that we need the file state_date to work
+
 class VirusPredictor
   #initialize sets our instance variables within a object
   def initialize(state_of_origin, population_density, population, region, regional_spread)
@@ -16,69 +16,39 @@ class VirusPredictor
     @region = region
     @next_region = regional_spread
   end
-  #virus_effects calls predicted_deaths and speed_of_spread
-  def virus_effects  #HINT: What is the SCOPE of instance variables?
+
+  def virus_effects #virus_effects calls predicted_deaths and speed_of_spread
     predicted_deaths
     speed_of_spread
   end
 
   private  #Everything under has to be called within the class
-# Its gonna predict number of deaths based on population density by muliplying population by given factor, for each state
-  def predicted_deaths
-    array  = [1,50,100,150,200].reverse
-    # number_of_deaths = 0
+
+  def predicted_deaths # Its gonna predict number of deaths based on population density by muliplying population by given factor, for each state
+   array  = [50,100,150,200].reverse
    array.each do |end_of_range|
-    # puts end_of_range
        if @population_density >= end_of_range
-        # puts "First statement"
-        # puts "population #{@population}"
-        # puts "population density #{@population_density}"
-        # puts "end of range #{end_of_range}"
          number_of_deaths = ( @population * end_of_range / 500 )
-         # puts  ( @population * end_of_range/500)
   print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-          break
-       # else
-       #   puts "Second statement"
-       #    number_of_deaths = (@population * 0.05).floor
-       #     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-       #     break
+         break
+       else
+         number_of_deaths = (@population * 0.05).floor
+  print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+         break
        end
-
     end
-    # if @population_density >= 200
-    #   number_of_deaths = (@population * 0.4).floor
-    # elsif @population_density >= 150
-    #   number_of_deaths = (@population * 0.3).floor
-    # elsif @population_density >= 100
-    #   number_of_deaths = (@population * 0.2).floor
-    # elsif @population_density >= 50
-    #   number_of_deaths = (@population * 0.1).floor
-    # else
-    #   number_of_deaths = (@population * 0.05).floor
-    # end
-
-
-
   end
-# speed_of_spread will calculate the spread of virus within a given state based on population density
-  def speed_of_spread #in months
-    speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
-    end
-
-    puts " and will spread across the state in #{speed} months.\n\n"
-
+  def speed_of_spread # speed_of_spread will calculate the spread of virus within a given state based on population density in months
+    speed = 2.5 # Instead of starting at 0 start at 2.5 which is the highest if can be
+    array_two  = [0,50,100,150,200].reverse # use the same array as in predicted_deaths because we are using @population_density as our common variable again
+    array_two.each do |end_of_range| # For each of the values in the Array_two we want to
+    if @population_density >= end_of_range # check to see if the @population_density vsalue is greater than each of the values
+       speed -= 0.5 * end_of_range/50 # Every time the value of @population_density is greater than the value of the end_of_range, the speed of the
+       break# so we dont add up all of the values of end_of_range and then substract
+     end
+   end
+    print " and will spread across the state in #{speed} months.\n"
   end
 
 end
@@ -90,7 +60,7 @@ end
 
 
 # alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population], STATE_DATA["Alabama"][:region], STATE_DATA["Alabama"][:regional_spread])
-# alabama.virus_effects
+# p alabama.virus_effects
 
 # jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population], STATE_DATA["New Jersey"][:region], STATE_DATA["New Jersey"][:regional_spread])
 # jersey.virus_effects
@@ -106,3 +76,10 @@ STATE_DATA.each do |key, value|
   report = VirusPredictor.new(key, STATE_DATA[key][:population_density], STATE_DATA[key][:population], STATE_DATA[key][:region], STATE_DATA[key][:regional_spread])
   report.virus_effects
 end
+
+
+
+
+
+
+
