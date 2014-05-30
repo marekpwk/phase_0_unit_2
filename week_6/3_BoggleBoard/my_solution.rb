148b9c927cd778166e1a3595843269bcf15f3d52
Mar <rkx1901@gmail.com>
2014-05-30 13:24:52 -0500

    Refactored and added test

--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -1,31 +1,13 @@
-# # U2.W6: Create a BoggleBoard Class
+require 'test/unit'
+# U2.W6: Create a BoggleBoard Class
+
+# I worked on this challenge [by myself, with: Jeremy].
 
 
-# # I worked on this challenge [by myself, with: ].
-
-
-# # 2. Pseudocode
-
-
-
-# # 3. Initial Solution
-# class BoggleBoard
-
-#   #your code here
-
-# end
-
-
-# dice_grid = [["b", "r", "a", "e"],
-#              ["i", "o", "d", "t"],
-#              ["e", "c", "l", "r"],
-#              ["t", "a", "k", "e"]]
-
-# boggle_board = BoggleBoard.new(dice_grid)
-
-
-
-# 4. Refactored Solution
+# 2. Pseudocode
+# Create a new method called get_diagonal that passes in row,column
+# We want to make sure tht first coordinate is a corner one like [0,0]
+#Create a string of diagnal coordinates
 
 
 
@@ -55,16 +37,13 @@ class BoggleBoard
   end
 
   def get_diagonal(row, column)
-    raise "Coordinates not top corners" unless (row == 0  && column == 0) || (row == 0  && column == @board.size-1)
+    raise "Coordinates not top corners" unless row == 0  && column == 0 || row == 0  && column == @board.size-1
     array =[]
-    index = column
-
     if column > 0
       4.times do |var|
         array << get_letter(row, column)
         row += 1
         column -= 1
-        puts index
       end
     else
       column = row
@@ -84,66 +63,90 @@ end
 
 # 4. Refactored Solution
 
-# class BoggleBoard
+class BoggleBoard
 
-#   def initialize(board)
-#     @board = board
-#   end
+  def initialize(board)
+    @board = board
+  end
 
-#   def create_word(*coords)
-#     coords.map { |coord| @board[coord.first][coord.last]}.join("")
-#   end
+  def create_word(*coords)
+    coords.map { |coord| @board[coord.first][coord.last]}.join("")
+  end
 
-#   def get_row(row)
-#     @board[row] == nil ? false : @board[row]
-#   end
+  def get_row(row)
+    @board[row] == nil ? false : @board[row]
+  end
 
-#   def get_column(column)
-#     return false if @board[column] == nil
-#     the_column = @board.collect { |num| num[column] }
-#   end
+  def get_column(column)
+    return false if @board[column] == nil
+    the_column = @board.collect { |num| num[column] }
+  end
 
-#   def get_letter(row, column)
-#     @board[row][column]
-#   end
-
-#   def get_diagonal(row, column)
-#     raise "Coordinates not top corners" unless (row == 0  && column == 0) || (row == 0  && column == @board.size-1)
-#     arr =[]
-#     index = column
-
-#     if column > 0
-#       4.times do |var|
-#         arr << get_letter(row, column)
-#         row += 1
-#         column -= 1
-#         puts index
-#       end
-#     else
-#       puts "else #{index}"
-#       column = row
-#       4.times do |var|
-#         arr << get_letter(row, column)
-#         row += 1
-#         column += 1
-#         puts index
-#       end
-#     end
-#     arr.inspect()
-#   end
+  def get_letter(row, column)
+    @board[row][column]
+  end
 
 
-# end
+  def get_diagonal(row, column)
+    raise ArgumentError,"Coordinates not top corners" unless row == 0  && column == 0 || row == 0  && column == @board.size - 1
+    array =[]
+    col_operator = "-"
+    (col_operator = "+"; column = row;) if column == 0
+     @board.size .times do |var|
+        array << get_letter(row, column)
+        row += 1
+        column = column.send(col_operator, 1)
+      end
+    array#.inspect()
+  end
+
+
+end
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-dice_grid = [["b", "r", "a", "e"],
-             ["i", "o", "d", "t"],
-             ["e", "c", "l", "r"],
-             ["t", "a", "k", "e"]]
 
-boggle_board = BoggleBoard.new(dice_grid)
-puts boggle_board.get_diagonal(0,0) == ["b", "o", "l", "e"]
-puts boggle_board.get_diagonal(0,3) == ["e", "d", "c", "t"]
-# puts boggle_board.get_diagonal(0,3) #== ["e", "d", "c", "t"]
+class BoggleBoardCheck < Test::Unit::TestCase
+  def setup
+    dice_grid = [["b", "r", "a", "e"],
+                 ["i", "o", "d", "t"],
+                 ["e", "c", "l", "r"],
+                 ["t", "a", "k", "e"]]
+
+    @boggle_board = BoggleBoard.new(dice_grid)
+  end
+
+  def test_get_diagonal_not_top_corners
+    assert_raise (ArgumentError) {@boggle_board.get_diagonal(1,3)}
+  end
+
+  def test_get_diagonal_from_right
+    assert_equal( ["e", "d", "c", "t"], @boggle_board.get_diagonal(0,3) )
+  end
+
+  def test_get_diagonal_from_left
+    assert_equal( ["b", "o", "l", "e"], @boggle_board.get_diagonal(0,0) )
+  end
+
+  def test_get_letter
+    assert_equal( "k", @boggle_board.get_letter(3,2) )
+  end
+
+  def test_get_row
+    assert_equal( ["e", "c", "l", "r"], @boggle_board.get_row(2) )
+  end
+
+  def test_get_row
+    assert_equal( ["e", "c", "l", "r"], @boggle_board.get_row(2) )
+  end
+
+  def test_get_column
+    assert_equal( ["a", "d", "l", "k"], @boggle_board.get_column(2) )
+  end
+
+  def test_create_word
+    assert_equal( "code", @boggle_board.create_word([2,1], [1,1], [1,2], [0,3]) )
+  end
+end
+# puts boggle_board.get_diagonal(1,3) #== ["e", "d", "c", "t"]
